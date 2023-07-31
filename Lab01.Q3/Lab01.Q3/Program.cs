9e09e3cbedc7d936f339f2c9e893b6652fe8f352
dilmimalisha <136615043+dilmimalisha@users.noreply.github.com>
2023-07-31 05:40:32 -0700

    Add files via upload

---
+++ Lab01.Q3/Lab01.Q3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab01.Q3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int value1;
+            Console.Write("Enter first value:");
+            value1 = int.Parse(Console.ReadLine());
+
+            int value2;
+            Console.Write("Enter second value:");
+            value2 = int.Parse(Console.ReadLine());
+
+            int sum = value1 + value2;
+            Console.WriteLine("Sum of the entered two values:" + sum);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Lab01.Q4/Lab01.Q4/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab01.Q4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Enter your salary:");
+            double salary = double.Parse(Console.ReadLine());
+            
+            
+            Console.Write("Enter the precentage of tax rate:");
+            double taxRate = double.Parse(Console.ReadLine());
+
+            double tax =salary*( taxRate / 100);
+            Console.WriteLine("Calculated tax:" + tax);
+
+            double grossSalary = salary- tax;
+            Console.WriteLine("The salary after  the tax:" +  grossSalary);
+
+       
+            Console.ReadLine();
+
+
+            
+
+        }
+    }
+}
