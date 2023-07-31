55dd4bd41a3fb228858c6d2bb0a415752fe6bb68
dilmimalisha <136615043+dilmimalisha@users.noreply.github.com>
2023-07-31 05:58:02 -0700

    Add files via upload

---
+++ Lab07.Q8/Lab07.Q8/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab07.Q8
+{
+    class Animal
+    {
+        public void Display()
+        {
+            Console.WriteLine("I am an Animal.");
+        }
+    }
+
+    class Dog : Animal
+    {
+        public void DisplayDogInfo()
+        {
+            Console.WriteLine("I have four legs.");
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            Dog dog = new Dog();
+            dog.Display();
+            dog.DisplayDogInfo();
+        }
+    }
+}
