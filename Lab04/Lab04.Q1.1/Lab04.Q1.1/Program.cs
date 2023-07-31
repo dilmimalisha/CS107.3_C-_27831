fe3b3c744e946183e20db466c3e498661c7fc751
dilmimalisha <136615043+dilmimalisha@users.noreply.github.com>
2023-07-31 05:51:10 -0700

    Add files via upload

---
+++ Lab04/Lab04.Q1.1/Lab04.Q1.1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab04.Q1._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ConvertValues converter = new ConvertValues();
+            converter.kilometerTOmeter();
+
+            Console.ReadLine();
+        }
+    }
+
+    class ConvertValues
+    {
+        public void kilometerTOmeter()
+        {
+            Console.Write("Enter the distance in kilometers: ");
+            double km = double.Parse(Console.ReadLine());
+
+            double m = km * 1000;
+            Console.WriteLine($"{km} kilometers is equal to {m} meters.");
+        }
+    }
+}
+
