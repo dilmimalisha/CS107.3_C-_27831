e33507fe5c9a0ee39d76477db75442d249700e34
dilmimalisha <136615043+dilmimalisha@users.noreply.github.com>
2023-07-31 05:56:10 -0700

    Add files via upload

---
+++ Lab06/Lab06.Q6/Lab06.Q6/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab06.Q6
+{
+    class CalculateValues
+    {
+        public int[] UserInput(int size)
+        {
+            int[] array = new int[size];
+
+            for (int i = 0; i < size; i++)
+            {
+                Console.WriteLine("Enter a number:");
+                int number = int.Parse(Console.ReadLine());
+                array[i] = number;
+            }
+
+            return array;
+        }
+    }
+
+    class MainClass
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the array size:");
+            int size = int.Parse(Console.ReadLine());
+
+            CalculateValues calculateValues = new CalculateValues();
+
+            int[] array = calculateValues.UserInput(size);
+
+            Console.WriteLine("The array elements are:");
+            for (int i = 0; i < size; i++)
+            {
+                Console.WriteLine(array[i]);
+            }
+        }
+    }
+
+}
---
+++ Lab06/Lab06.Q7/Lab06.Q7/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab06.Q7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the size of the arrays:");
+            int size = int.Parse(Console.ReadLine());
+
+            int[] array1 = new int[size];
+            int[] array2 = new int[size];
+
+            Console.WriteLine("Enter values for the first array:");
+            for (int i = 0; i < size; i++)
+            {
+                Console.WriteLine($"Enter value {i + 1}:");
+                array1[i] = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("Enter values for the second array:");
+            for (int i = 0; i < size; i++)
+            {
+                Console.WriteLine($"Enter value {i + 1}:");
+                array2[i] = int.Parse(Console.ReadLine());
+            }
+
+            // Scalar Sum
+            int scalarSum = 0;
+            for (int i = 0; i < size; i++)
+            {
+                scalarSum += array1[i] + array2[i];
+            }
+
+            Console.WriteLine("Scalar Sum:");
+            Console.WriteLine(scalarSum);
+
+            // Vector Sum
+            int[] vectorSum = new int[size];
+            for (int i = 0; i < size; i++)
+            {
+                vectorSum[i] = array1[i] + array2[i];
+            }
+
+            Console.WriteLine("Vector Sum:");
+            PrintArray(vectorSum);
+
+            // Vector Product
+            int[] vectorProduct = new int[size];
+            for (int i = 0; i < size; i++)
+            {
+                vectorProduct[i] = array1[i] * array2[i];
+            }
+
+            Console.WriteLine("Vector Product:");
+            PrintArray(vectorProduct);
+
+            // Scalar Product
+            int scalarProduct = 0;
+            for (int i = 0; i < size; i++)
+            {
+                scalarProduct += array1[i] * array2[i];
+            }
+
+            Console.WriteLine("Scalar Product:");
+            Console.WriteLine(scalarProduct);
+        }
+
+        static void PrintArray(int[] arr)
+        {
+            foreach (int value in arr)
+            {
+                Console.Write(value + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
