158878ceda3870ea5210e5f783be4f0c96b363cf
dilmimalisha <136615043+dilmimalisha@users.noreply.github.com>
2023-07-31 05:19:19 -0700

    Add files via upload

---
+++ Lab01.Q1/Lab01.Q1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab01.Q1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string name;
+            Console.Write("Enter your name: ");
+            name = Console.ReadLine();
+            
+
+            Console.Write("Enter your batch: ");
+            double batch = double.Parse(Console.ReadLine());
+
+
+            Console.WriteLine("My name is " + name);
+            Console.WriteLine("Batch " +  batch);
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Lab01.Q2/Lab01.Q2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab01.Q2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Enter the radius:");
+            double radius = double.Parse(Console.ReadLine());
+
+            double pi = 3.142;
+            double area;
+
+            area = pi*radius*radius;
+            Console.WriteLine("Area of the circle:" +  area);
+
+            Console.ReadLine();
+
+        }
+    }
+}
